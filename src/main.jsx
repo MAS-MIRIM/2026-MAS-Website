187dd25ecd9b07d9b4b388cf5ccdb333778cbd7c
hjzhhhj <jhj090120@gmail.com>
2026-03-18 10:06:48 +0900

    [Fix] :: Prize 화면 오류 수정

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { registerSW } from 'virtual:pwa-register';
+import "./styles/global.css";
 
 registerSW({ immediate: true });
 
